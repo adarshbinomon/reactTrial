8a26cc89113dc2611afaf57d0cfad55ab88d4f2b
Adarsh Binomon <adarshbinomon@adarshs-MacBook-Pro.local>
2023-10-28 06:54:41 +0530

    chapter 3

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", { id: "title" }, "Heading 1");
-const heading1 = React.createElement("h2", { id: "title" }, "Heading 2");
-const container = React.createElement("div", { id: "title" }, [heading, heading1]);
+const heading1 = (
+    <h1 id="title">
+        hello gooys!
+    </h1>
+)
 
 console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(heading);
